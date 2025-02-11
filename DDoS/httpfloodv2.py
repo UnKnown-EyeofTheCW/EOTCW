23af0600033113065b373e2f1f55e60d61a10258
UnKnown-EyeofTheCW <chrispelino33@gmail.com>
2025-02-11 11:47:24 +0800

    initial commit

--- DDoS/httpfloodv2.py
+++ DDoS/httpfloodv2.py
@@ -1,4 +1,6 @@
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 import time
 import random
 import sys
